898d2167382aeb387afb025855c68bf9820d4bfe
Alexander <alexander_deazevedo@hotmail.com>
2017-06-15 17:09:31 -0400

    holaaaa

---
+++ app/src/main/java/com/ids/appsubasta/subasta/Adaptador.java
@@ -0,0 +1,56 @@
+package com.ids.appsubasta.subasta;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ids.appsubasta.subasta.Bien.Bienes;
+
+import java.util.List;
+
+import static com.ids.appsubasta.subasta.R.id.parent;
+
+/**
+ * Created by Usuario on 15/06/2017.
+ */
+
+public class Adaptador extends RecyclerView.Adapter<Adaptador.adaptadorHolder>{
+
+private List<Bienes> bienes;
+    public Adaptador (List<Bienes> bienes){
+        this.bienes = bienes;
+    }
+
+    @Override
+    public adaptadorHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_item, parent, false);
+        return new adaptadorHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(adaptadorHolder adaptadorViewHolder, int position) {
+       Bienes bien = bienes.get(position);
+        adaptadorViewHolder.texto.setText(bien.getNombre());
+        adaptadorViewHolder.descripcion.setText(bien.getDescripcion());
+    }
+
+    @Override
+    public int getItemCount() {
+        return bienes.size();
+    }
+
+    public class adaptadorHolder extends RecyclerView.ViewHolder{
+        private TextView texto, descripcion;
+        private ImageView imagen;
+
+        public adaptadorHolder(View itemView){
+            super(itemView);
+            texto = (TextView)itemView.findViewById(R.id.textoid);
+            imagen = (ImageView) itemView.findViewById(R.id.imagenid);
+        }
+    }
+}
--- app/src/main/java/com/ids/appsubasta/subasta/Bien/Bienes.java
+++ app/src/main/java/com/ids/appsubasta/subasta/Bien/Bienes.java
@@ -5,4 +5,48 @@ package com.ids.appsubasta.subasta.Bien;
  */
 
 public class Bienes {
+
+    private String nombre;
+    private String descripcion;
+    private String identificacion;
+    private int foto;
+
+    public Bienes (String nombre, String descripcion, String identificacion, int foto){
+        this.nombre= nombre;
+        this.descripcion= descripcion;
+        this.identificacion= identificacion;
+        this.foto= foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
 }
