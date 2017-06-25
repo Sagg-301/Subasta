eba3fb8c6135c705571d6ac6e02d70f7522d5b2b
Sergio Garcia <sagg_301@outlook.com>
2017-06-24 23:27:38 -0400

    +Realm funcional!

---
+++ app/src/main/java/com/ids/appsubasta/subasta/Fase/Publicidad.java
@@ -0,0 +1,15 @@
+package com.ids.appsubasta.subasta.Fase;
+
+import com.ids.appsubasta.subasta.Subasta;
+
+/**
+ * Created by Sergio on 23/6/2017.
+ */
+
+public class Publicidad implements Fase {
+
+    @Override
+    public void cambiarFase(Subasta subasta) {
+        subasta.setFase(this);
+    }
+}
